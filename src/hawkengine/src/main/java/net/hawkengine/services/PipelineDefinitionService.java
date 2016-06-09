c258b416ddce1443769dd27fd68c4a42d78a8bef
monitobeko <monitobeko@abv.bg>
2016-06-09 14:56:33 +0300

    Small refactoring to Pipeline Definition Service #2

--- src/hawkengine/src/main/java/net/hawkengine/services/PipelineDefinitionService.java
+++ src/hawkengine/src/main/java/net/hawkengine/services/PipelineDefinitionService.java
@@ -21,22 +21,27 @@ public class PipelineDefinitionService extends CrudService<PipelineDefinition> i
         super.repository = repository;
     }
 
+    @Override
     public ServiceResult getById(String pipelineDefinitionId) {
         return super.getById(pipelineDefinitionId);
     }
 
+    @Override
     public ServiceResult getAll() {
         return super.getAll();
     }
 
+    @Override
     public ServiceResult add(PipelineDefinition pipelineDefinition) {
         return super.add(pipelineDefinition);
     }
 
+    @Override
     public ServiceResult update(PipelineDefinition pipelineDefinition) {
         return super.update(pipelineDefinition);
     }
 
+    @Override
     public ServiceResult delete(String pipelineDefinitionId) {
         return super.delete(pipelineDefinitionId);
     }
