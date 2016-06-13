3f2588c6ac4721a10f4099542bea6ba8e03e03a1
Boris Petrov <bpetrov@rnd-solutions.net>
2016-06-13 15:25:12 +0300

    updating before pull

--- src/hawkengine/src/main/java/net/hawkengine/core/SchemaValidator.java
+++ src/hawkengine/src/main/java/net/hawkengine/core/SchemaValidator.java
@@ -1,6 +1,9 @@
 package net.hawkengine.core;
 
+import net.hawkengine.model.Agent;
+import net.hawkengine.model.JobDefinition;
 import net.hawkengine.model.MaterialDefinition;
+import net.hawkengine.model.MaterialType;
 import net.hawkengine.model.PipelineGroup;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,7 +14,9 @@ import java.util.regex.Pattern;
 public class SchemaValidator {
     private String message = "OK";
     private static final String NAME_PATTERN = "[A-Za-z]{3,20}"; //TODO enter name pattern
-    private static final String URL_PATTERN = ""; //TODO enter url pattern
+    private static final String GIT_PATTERN = "[a-zA-Z]{5,50}"; //TODO define git url pattern
+    private static final String NUGET_PATTERN = "[a-zA-Z]{5,50}"; //TODO define git url pattern
+
 
 
     //PIPELINEGROUP
@@ -19,17 +24,47 @@ public class SchemaValidator {
         if (pipelineGroup != null) {
             String name = pipelineGroup.getName();
                 if (name == null) {
-                   return this.message = "ERROR: PIPELINE GROUP NAME IS NULL";
+                   return this.message = "ERROR: PIPELINEGROUP NAME IS NULL.";
                 }
 
                 if(isValidRegEx(name,NAME_PATTERN) == false){
-                    return this.message = "ERROR: Pipeline Group name is invalid.";
+                    return this.message = "ERROR: PIPELINEGROUP NAME IS INVALID.";
                 }
         }
         else {
-            return this.message = "ERROR: PIPELINEGROUP IS NULL";
+            return this.message = "ERROR: PIPELINEGROUP IS NULL.";
         }
         return message;
+    }
+
+    //AGENT
+    public String validate(Agent agent){
+        if (agent != null){
+            String agentName = agent.getName();
+            String hostname = agent.getHostName();
+            String ipAdress = agent.getIPAddress();
+
+
+
+            if (agentName == null){
+                return this.message = "ERROR: AGENT NAME IS NULL.";
+            }
+
+            if (this.isValidRegEx(agentName,NAME_PATTERN) == false){
+                return this.message = "ERROR: AGENT NAME IS INVALID.";
+            }
+
+            if (hostname == null){
+                return this.message = "ERROR: AGENT HOSTNAME IS NULL.";
+            }
+
+            if (this.isValidRegEx(hostname,NAME_PATTERN) == false){
+                return this.message = "ERROR: AGENT HOSTNAME IS INVALID.";
+            }
+
+
+        }
+        return this.message;
     }
 
     //MATERIAL DEFINITION
@@ -39,26 +74,33 @@ public class SchemaValidator {
             String materialURL = materialDefinition.getUrl();
             String materialPipeline = materialDefinition.getPipelineName();
             if (materialName == null){
-                return this.message = "ERROR: Material Definition name can not be null.";
+                return this.message = "ERROR: MATERIAL DEFINITION NAME IS NULL.";
             }
-            if (isValidRegEx(materialName, NAME_PATTERN) == false){
-                return this.message = "ERROR: Material name is ivalid.";
+            if (this.isValidRegEx(materialName, NAME_PATTERN) == false){
+                return this.message = "ERROR: MATERIAL DEFINITION NAME IS INVALID.";
             }
-            if (materialURL == null){
+            if (materialURL != null){
+                if(materialDefinition.getType() == MaterialType.GIT){
+                    if(this.isValidRegEx(materialURL, GIT_PATTERN) == false){
+                       return this.message = "ERROR: INVALID GIT URL.";
+                    }
+                } else if (materialDefinition.getType() == MaterialType.NUGET){
+                    if (this.isValidRegEx(materialURL, NUGET_PATTERN) == false){
+                       return this.message = "ERROR: INVALID NUGET URL.";
+                    }
+                }else {
+                    return this.message = "ERROR: MATERIAL URL IS INVALID.";
+                }
+            } else {
                 return this.message = "ERROR: Material URL is NULL.";
             }
-            if (isValidRegEx(materialDefinition.getUrl(),URL_PATTERN) == false){
-                return this.message = "ERROR: INVALID MATERIAL URL";
-            }
+
             if (materialPipeline == null){
-                return this.message = "ERROR: Material ID name can not be null.";
-            }
-            if (isValidRegEx(materialPipeline, NAME_PATTERN) == false){
-                return this.message = "ERROR: Material ID is ivalid.";
+                return this.message = "ERROR: MATERIAL PIPELINE ID CAN NOT BE NULL.";
             }
 
         } else {
-            message = "ERROR: Material Definition is NULL";
+            return this.message = "ERROR: Material Definition is NULL";
         }
         return message;
     }
