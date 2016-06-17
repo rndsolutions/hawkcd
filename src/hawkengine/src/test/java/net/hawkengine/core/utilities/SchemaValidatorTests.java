ce193b4770710a82b253ab55522085e01cfbd52a
Boris Petrov <boris.petrov88@abv.bg>
2016-06-17 18:05:49 +0300

    implemented tests for all task definitions. #4

--- src/hawkengine/src/test/java/net/hawkengine/core/utilities/SchemaValidatorTests.java
+++ src/hawkengine/src/test/java/net/hawkengine/core/utilities/SchemaValidatorTests.java
@@ -1,24 +1,27 @@
 package net.hawkengine.core.utilities;
-import net.hawkengine.core.SchemaValidator;
+import com.sun.jmx.snmp.tasks.TaskServer;
+
+import net.hawkengine.http.Exec;
+import net.hawkengine.model.Agent;
+import net.hawkengine.model.Environment;
 import net.hawkengine.model.ExecTask;
+import net.hawkengine.model.FetchArtifactTask;
+import net.hawkengine.model.FetchMaterialTask;
 import net.hawkengine.model.JobDefinition;
 import net.hawkengine.model.MaterialDefinition;
-import net.hawkengine.model.Stage;
+import net.hawkengine.model.Task;
+import net.hawkengine.model.UploadArtifactTask;
 import net.hawkengine.model.enums.MaterialType;
-import net.hawkengine.model.Pipeline;
 import net.hawkengine.model.PipelineDefinition;
 import net.hawkengine.model.PipelineGroup;
-import net.hawkengine.model.enums.RunIf;
 import net.hawkengine.model.StageDefinition;
-import net.hawkengine.model.Task;
 import net.hawkengine.model.TaskDefinition;
+import net.hawkengine.model.enums.RunIf;
 import net.hawkengine.model.enums.TaskType;
 
 import org.junit.Test;
 
-import java.nio.channels.Pipe;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
@@ -36,7 +39,8 @@ public class SchemaValidatorTests {
     private List <MaterialDefinition> material = new ArrayList<MaterialDefinition>();
     private List <StageDefinition> stage = new ArrayList<StageDefinition>();
     private List <JobDefinition> job = new ArrayList<JobDefinition>();
-    private List <TaskDefinition> task = new ArrayList<TaskDefinition>();
+   // private List <TaskDefinition> task = new ArrayList<TaskDefinition>();
+    private List <TaskDefinition> execTaskList = new ArrayList<TaskDefinition>();
 
 
     //Arranging null objects
@@ -46,7 +50,10 @@ public class SchemaValidatorTests {
     private StageDefinition stageDefinition;
     private JobDefinition jobDefinition;
     private TaskDefinition taskDefinition;
+    private ExecTask execTask;
+    private UploadArtifactTask uploadArtifactTask;
     private MaterialDefinition materialDefinition;
+    ;
     //--------------------------------------------------
 
 
@@ -100,8 +107,6 @@ public class SchemaValidatorTests {
     public void validate_PipelineGroup_Name_RegExMismatch(){
         //Arrange
         PipelineGroup pipelineGroup = new PipelineGroup();
-        //pipelineGroup.setName(" "); //too short name
-        //pipelineGroup.setName("this name is too long");
         pipelineGroup.setName("#$!13das");
         String expectedResult = "ERROR: PIPELINEGROUP NAME IS INVALID.";
 
@@ -119,25 +124,37 @@ public class SchemaValidatorTests {
     @Test
     public void validate_PipelineDefinition(){
         //Arrange
+
+        MaterialDefinition materialDefinition = new MaterialDefinition();
+        materialDefinition.setName("materialName");
+        materialDefinition.setType(MaterialType.GIT);
+        materialDefinition.setUrl("http://");
+        materialDefinition.setPipelineDefinitionId("MaterialPipelineId");
+        this.material.add(0,materialDefinition);
+
+        ExecTask execTask = new ExecTask();
+        ArrayList arguments = new ArrayList();
+        arguments.add("command1");
+        execTask.setName("execTask");
+        execTask.setRunIfCondition(RunIf.ANY);
+        execTask.setCommand("start");
+        execTask.setArguments(arguments);
+        execTaskList.add(execTask);
+
+        JobDefinition jobDefinition = new JobDefinition();
+        jobDefinition.setName("jobDefinition");
+        jobDefinition.setTaskDefinitions(execTaskList);
+        job.add(0,jobDefinition);
+
+        StageDefinition stageDefinition = new StageDefinition();
+        stageDefinition.setName("stageName");
+        stageDefinition.setJobDefinitions(job);
+        this.stage.add(0,stageDefinition);
+
         PipelineDefinition pipelineDefinition = new PipelineDefinition();
         pipelineDefinition.setName("pipelineDefinition");
-        pipelineDefinition.setPipelineGroupId("pipelineGroupId");
-
-        MaterialDefinition mt1 = new MaterialDefinition();
-        mt1.setName("materialName");
-        mt1.setType(MaterialType.GIT);
-        mt1.setUrl("dasdasdas");
-        mt1.setPipelineDefinitionId("dasdasda");
-        this.material.add(0,mt1);
-        pipelineDefinition.setMaterials(material);
-        StageDefinition st1 = new StageDefinition();
-        st1.setName("stageName");
-        job.add(0,new JobDefinition());
-        st1.setJobDefinitions(job);
-        this.stage.add(0,st1);
-
         pipelineDefinition.setStageDefinitions(stage);
-
+        pipelineDefinition.setMaterials(this.material);
 
         //Act
         String actualResult = this.validator.validate(pipelineDefinition);
@@ -194,21 +211,6 @@ public class SchemaValidatorTests {
     }
 
     @Test
-    public void validate_PipelineDefinition_PipelineGroup_Null(){
-        //Arrange
-        PipelineDefinition pipelineDefinition = new PipelineDefinition();
-        pipelineDefinition.setName("pipelineDefinition");
-        String expectedResult = "ERROR: PIPELINE GROUP ID IS NULL.";
-
-        //Act
-        String actualResult = this.validator.validate(pipelineDefinition);
-
-        //Assert
-        assertNotNull(actualResult);
-        assertEquals(expectedResult,actualResult);
-    }
-
-    @Test
     public void validate_PipelineDefinition_Materials(){
         //Arrange
         PipelineDefinition pipelineDefinition = new PipelineDefinition();
@@ -256,7 +258,18 @@ public class SchemaValidatorTests {
         StageDefinition stageDefinition = new StageDefinition();
         stageDefinition.setName("StageDefinition");
         stageDefinition.setPipelineDefinitionId("pipelineDefinition");
-        job.add(0, new JobDefinition());
+        JobDefinition jobDefinition = new JobDefinition();
+        jobDefinition.setName("jobDefinition");
+        ExecTask execTask = new ExecTask();
+        ArrayList arguments = new ArrayList();
+        arguments.add("command1");
+        execTask.setName("execTask");
+        execTask.setRunIfCondition(RunIf.ANY);
+        execTask.setCommand("start");
+        execTask.setArguments(arguments);
+        execTaskList.add(execTask);
+        jobDefinition.setTaskDefinitions(execTaskList);
+        job.add(0,jobDefinition);;
         stageDefinition.setJobDefinitions(job);
 
         //Act
@@ -313,28 +326,12 @@ public class SchemaValidatorTests {
     }
 
     @Test
-    public void validate_StageDefinition_PipelineId_Null(){
-        //Arrange
-        StageDefinition stageDefinition = new StageDefinition();
-        stageDefinition.setName("stageDefinitionName");
-        String exptectedResult = "ERROR: PIPELINE DEFINITION ID IS NULL.";
-
-        //Act
-        String actualResult = this.validator.validate(stageDefinition);
-
-        //Assert
-        assertNotNull(actualResult);
-        assertEquals(exptectedResult,actualResult);
-
-    }
-
-    @Test
     public void validate_StageDefinition_Job_Null(){
         //Arrange
         StageDefinition stageDefinition = new StageDefinition();
         stageDefinition.setName("stageDefinitionName");
         stageDefinition.setPipelineDefinitionId("pipelineDefinitionId");
-        String exptectedResult = "ERROR: JOB NOT ADDED.";
+        String exptectedResult = "ERROR: STAGE DEFINITION JOB NOT ADDED.";
 
         //Act
         String actualResult = this.validator.validate(stageDefinition);
@@ -356,8 +353,15 @@ public class SchemaValidatorTests {
         JobDefinition jobDefinition = new JobDefinition();
         jobDefinition.setName("jobDefinition");
         jobDefinition.setStageDefinitionId("stageDefinitionId");
-        task.add(0, new ExecTask());
-        jobDefinition.setTaskDefinitions(task);
+        ExecTask execTask = new ExecTask();
+        ArrayList arguments = new ArrayList();
+        arguments.add("command1");
+        execTask.setName("execTask");
+        execTask.setRunIfCondition(RunIf.ANY);
+        execTask.setCommand("start");
+        execTask.setArguments(arguments);
+        execTaskList.add(execTask);;
+        jobDefinition.setTaskDefinitions(execTaskList);
 
         //Act
         String actualResult = this.validator.validate(jobDefinition);
@@ -409,20 +413,6 @@ public class SchemaValidatorTests {
         assertEquals(expectedResult,actualResult);
     }
 
-    @Test
-    public void validate_JobDefinition_SageId_Null(){
-        //Arrange
-        JobDefinition jobDefinition = new JobDefinition();
-        jobDefinition.setName("jobDefinitionName");
-        String expectedResult = "ERROR: STAGE DEFINITION ID IS NULL.";
-
-        //Act
-        String actualResult = this.validator.validate(jobDefinition);
-
-        //Assert
-        assertNotNull(actualResult);
-        assertEquals(expectedResult,actualResult);
-    }
 
     @Test
     public void validate_JobDefinition_Task_Null(){
@@ -442,21 +432,331 @@ public class SchemaValidatorTests {
 //--------------------------------------------------------------------------------------------------
 
 
-    //----------------------------------------------------------------------------------------------
-    //Tasks TESTS
-
-
 
 
     //----------------------------------------------------------------------------------------------
+    //Tasks TEST
+
+    @Test
+    public void validate_ExecTask(){
+        //Arrange
+        ExecTask execTask = new ExecTask();
+        execTask.setName("execTask");
+        execTask.setRunIfCondition(RunIf.PASSED);
+        execTask.setCommand("kill");
+        ArrayList argumentsList = new ArrayList();
+        argumentsList.add("killAll");
+        execTask.setArguments(argumentsList);
+
+        //Act
+        String actualReult = this.validator.validate(execTask);
+
+        //Assert
+        assertEquals(expectedResult,actualReult);
+    }
+
+
+    @Test
+    public void validate_ExecTask_Commnad_Null(){
+        //Arrange
+        ExecTask execTask = new ExecTask();
+        execTask.setName("taskDefinition");
+        execTask.setRunIfCondition(RunIf.PASSED);
+        String expectedResult = "ERROR: TASK COMMAND IS NULL.";
+
+        //Act
+        String actualReult = this.validator.validate(execTask);
+
+        //Assert
+        assertEquals(expectedResult,actualReult);
+    }
+
+    @Test
+    public void validate_ExecTask_ArgumentList_Null(){
+        //Arrange
+        ExecTask execTask = new ExecTask();
+        execTask.setName("taskDefinition");
+        execTask.setRunIfCondition(RunIf.PASSED);
+        execTask.setCommand("kill");
+        String expectedResult = "ERROR TASK ARGUMENTS LIST IS NULL.";
+
+        //Act
+        String actualReult = this.validator.validate(execTask);
+
+        //Assert
+        assertEquals(expectedResult,actualReult);
+    }
+
+    @Test
+    public void validate_ExecTask_ArgumentList_Empty(){
+        //Arrange
+        ExecTask execTask = new ExecTask();
+        execTask.setName("taskDefinition");
+        execTask.setRunIfCondition(RunIf.PASSED);
+        execTask.setCommand("kill");
+        execTask.setCommand("kill");
+        execTask.setArguments(new ArrayList());
+        String expectedResult = "ERROR: TASK ARGUMENT LIST IS EMPTY.";
+
+        //Act
+        String actualReult = this.validator.validate(execTask);
+
+        //Assert
+        assertEquals(expectedResult,actualReult);
+    }
+
+    @Test
+    public void validate_FetchArtifactTask(){
+        //Arrange
+        FetchArtifactTask fetchArtifactTask = new FetchArtifactTask();
+        fetchArtifactTask.setName("taskDefinition");
+        fetchArtifactTask.setRunIfCondition(RunIf.PASSED);
+        fetchArtifactTask.setPipeline("pipelineName");
+        fetchArtifactTask.setStage("pipelineStage");
+        fetchArtifactTask.setJob("pipelineJob");
+        fetchArtifactTask.setSource("sourceFolder");
+        fetchArtifactTask.setDestination("destinationFolder");
+
+
+        //Arrange
+        String actualResult = this.validator.validate(fetchArtifactTask);
+
+        //Assert
+        assertEquals(this.expectedResult,actualResult);
+
+    }
+
+
+
+
+
+    @Test
+    public void validate_FetchMaterialtTask(){
+        //Arrange
+        FetchMaterialTask fetchMaterialTask = new FetchMaterialTask();
+        fetchMaterialTask.setName("taskDefinition");
+        fetchMaterialTask.setRunIfCondition(RunIf.PASSED);
+        fetchMaterialTask.setMaterialName("materialName");
+        fetchMaterialTask.setPipelineName("pipelineName");
+        fetchMaterialTask.setSource("source");
+        fetchMaterialTask.setDestination("destination");
+
+
+        //Arrange
+        String actualResult = this.validator.validate(fetchMaterialTask);
+
+        //Assert
+        assertEquals(this.expectedResult,actualResult);
+
+    }
+
+
+
+    @Test
+    public void validate_UploadArtifactTask(){
+        //Arrane
+        UploadArtifactTask uploadArtifactTask = new UploadArtifactTask();
+        uploadArtifactTask.setName("taskDefinition");
+        uploadArtifactTask.setRunIfCondition(RunIf.PASSED);
+        uploadArtifactTask.setSource("/sourceFolder");
+        uploadArtifactTask.setDestination("/destinationFolder");
+
+        //Act
+        String actualResult = this.validator.validate(uploadArtifactTask);
+
+        //Arrange
+        assertNotNull(actualResult);
+        assertEquals(this.expectedResult,actualResult);
+    }
+
+
+    @Test
+    public void validate_UploadArtifactTask_Source(){
+        //Arrane
+        UploadArtifactTask uploadArtifactTask = new UploadArtifactTask();
+        uploadArtifactTask.setName("taskDefinition");
+        uploadArtifactTask.setRunIfCondition(RunIf.PASSED);
+        String expectedResult = "ERROR: UPLOAD ARTIFACT TASK SOURCE FOLDER IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(uploadArtifactTask);
+
+        //Arrange
+        assertNotNull(actualResult);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_UploadArtifactTask_Destination(){
+        //Arrane
+        UploadArtifactTask uploadArtifactTask = new UploadArtifactTask();
+        uploadArtifactTask.setName("taskDefinition");
+        uploadArtifactTask.setRunIfCondition(RunIf.PASSED);
+        uploadArtifactTask.setSource("/source");
+        String expectedResult = "ERROR: UPLOAD ARTIFACT TASK DESTINATION FOLDER IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(uploadArtifactTask);
+
+        //Arrange
+        assertNotNull(actualResult);
+        assertEquals(expectedResult,actualResult);
+    }
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+    //Agent TESTS
+    @Test
+    public void validate_Agent(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("agent");
+        agent.setHostName("hostname");
+        agent.setIpAddress("ipadress");
+        agent.setRootPath("path");
+        agent.setOperatingSystem("debian");
+        agent.setEnvironment(new Environment());
+
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+
+    @Test
+    public void validate_Agent_Name_Null(){
+        //Arrange
+        Agent agent = new Agent();;
+        String expectedResult = "ERROR: AGENT NAME IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_Agent_Name_RegExMismatch(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("##$@#");
+        String expectedResult = "ERROR: AGENT NAME IS INVALID.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_Agent_Hostname_Null(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("agentBro");
+        String expectedResult = "ERROR: AGENT HOSTNAME IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_Agent_IPAdress(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("agentBro");
+        agent.setHostName("hostname");
+        String expectedResult = "ERROR: AGENT IP ADRESS IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_Agent_RootPath(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("agentBro");
+        agent.setHostName("hostname");
+        agent.setIpAddress("ipadress");
+        String expectedResult = "ERROR: AGENT ROOT PATH IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_Agent_OS(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("agentBro");
+        agent.setHostName("hostname");
+        agent.setIpAddress("ipadress");
+        agent.setRootPath("/path");
+        String expectedResult = "ERROR: AGENT OPERATIONAL SYSTEM IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void validate_Agent_Env(){
+        //Arrange
+        Agent agent = new Agent();
+        agent.setName("agentBro");
+        agent.setHostName("hostname");
+        agent.setIpAddress("ipadress");
+        agent.setRootPath("/path");
+        agent.setOperatingSystem("mindows");
+        String expectedResult = "ERROR: AGENT ENVIRONMENT IS NULL.";
+
+        //Act
+        String actualResult = this.validator.validate(agent);
+
+        //Assert
+        assertNotNull(agent);
+        assertEquals(expectedResult,actualResult);
+    }
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------
     //MaterialDefinition TESTS
     @Test
     public void validate_MaterialDefinition() throws Exception {
         //Arrange
         MaterialDefinition materialDefinition = new MaterialDefinition();
         materialDefinition.setName("materialDefinition");
-        materialDefinition.setUrl("materialUrl");
         materialDefinition.setType(MaterialType.GIT);
+        materialDefinition.setUrl("http://");
         materialDefinition.setPipelineDefinitionId("materialPipeline");
 
 
