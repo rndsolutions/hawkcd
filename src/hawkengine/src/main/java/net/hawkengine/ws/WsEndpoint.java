6b59f9e7df7462bbef980d9afd9c0c289915b898
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-06-21 11:57:07 +0300

    Added the SchemaValidator to the flow of the endpoint #4

--- src/hawkengine/src/main/java/net/hawkengine/ws/WsEndpoint.java
+++ src/hawkengine/src/main/java/net/hawkengine/ws/WsEndpoint.java
@@ -4,14 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
-
+import net.hawkengine.core.utilities.SchemaValidator;
 import net.hawkengine.core.utilities.constants.LoggerMessages;
 import net.hawkengine.core.utilities.deserializers.TaskDefinitionDeserializer;
 import net.hawkengine.core.utilities.deserializers.WsContractDeserializer;
+import net.hawkengine.model.Server;
 import net.hawkengine.model.ServiceResult;
 import net.hawkengine.model.TaskDefinition;
+import net.hawkengine.model.dto.ConversionObject;
 import net.hawkengine.model.dto.WsContractDto;
-
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.websocket.api.RemoteEndpoint;
 import org.eclipse.jetty.websocket.api.Session;
@@ -64,6 +65,20 @@ public class WsEndpoint extends WebSocketAdapter {
                 contract.setErrorMessage("Invalid Json was provided");
                 remoteEndpoint.sendString(serializer.toJson(contract));
                 return;
+            }
+
+            SchemaValidator schemaValidator = new SchemaValidator();
+            for (ConversionObject conversionObject : contract.getArgs()) {
+                Class objectClass = Class.forName(conversionObject.getPackageName());
+                Object object = this.jsonConverter.fromJson(conversionObject.getObject(), objectClass);
+                String result = schemaValidator.validate(object);
+
+                if (!result.equals("OK")) {
+                    contract.setError(true);
+                    contract.setErrorMessage(result);
+                    remoteEndpoint.sendString(serializer.toJson(contract));
+                    return;
+                }
             }
 
 			ServiceResult result = (ServiceResult) this.call(contract);
