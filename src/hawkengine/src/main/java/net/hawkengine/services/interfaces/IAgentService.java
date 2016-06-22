3256e30f1cc892d6aa870c36ba23eba3c795f0ee
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-06-22 13:17:35 +0300

    Added getAllAssignableAgents method #10

--- src/hawkengine/src/main/java/net/hawkengine/services/interfaces/IAgentService.java
+++ src/hawkengine/src/main/java/net/hawkengine/services/interfaces/IAgentService.java
@@ -4,22 +4,5 @@ import net.hawkengine.model.Agent;
 import net.hawkengine.model.ServiceResult;
 
 public interface IAgentService extends ICrudService<Agent> {
-    @Override
-    ServiceResult getById(String agentId);
-
-    @Override
-    ServiceResult getAll();
-
-    @Override
-    ServiceResult add(Agent agent);
-
-    @Override
-    ServiceResult update(Agent agent);
-
-    @Override
-    ServiceResult delete(String agentId);
-
-    ServiceResult getAllEnabledAgents();
-
-    ServiceResult getAllEnabledIdleAgents();
+    ServiceResult getAllAssignableAgents();
 }
