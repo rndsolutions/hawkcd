62dc28905a6669aefcb891a9f3919024ecc0462a
Boris Petrov <boris.petrov88@abv.bg>
2016-06-27 19:39:54 +0300

    feature - added automatic endpoint finder class.

---
+++ src/hawkengine/src/main/java/net/hawkengine/core/utilities/EndpointFinder.java
@@ -0,0 +1,29 @@
+package net.hawkengine.core.utilities;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EndpointFinder {
+    private static final char PKG_SEPARATOR = '.';
+    private static final char DIR_SEPARATOR = '/';
+    private static final String CLASS_FILE_SUFFIX = ".class";
+    private String endpoints ="";
+    public String getClasses(String packageName){
+        List <String> nameList = new ArrayList<String>();
+        String scannedPath = packageName.replace(PKG_SEPARATOR, DIR_SEPARATOR);
+        URL scannedUrl = Thread.currentThread().getContextClassLoader().getResource(scannedPath);
+        File scannedDir = new File(scannedUrl.getFile());
+        for (File file : scannedDir.listFiles()){
+            if(file.getAbsolutePath().endsWith(CLASS_FILE_SUFFIX)) {
+                int endIndex = file.getName().length() - CLASS_FILE_SUFFIX.length();
+                String className = file.getName().substring(0, endIndex);
+                String finished = packageName+"."+className+", ";
+                this.endpoints += finished;
+                nameList.add(finished);
+            }
+        }
+        return endpoints;
+    }
+}
