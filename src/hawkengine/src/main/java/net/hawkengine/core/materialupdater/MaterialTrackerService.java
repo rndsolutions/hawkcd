c2167409744f95881be1bcea22002df482e8954c
vladislavnikolov <vventsislavov@rnd-solutions.net>
2016-07-06 13:06:59 +0300

    Implemented additional logic #46

--- src/hawkengine/src/main/java/net/hawkengine/core/materialupdater/MaterialTrackerService.java
+++ src/hawkengine/src/main/java/net/hawkengine/core/materialupdater/MaterialTrackerService.java
@@ -1,14 +1,24 @@
 package net.hawkengine.core.materialupdater;
 
+import net.hawkengine.model.MaterialDefinition;
 import net.hawkengine.model.PipelineDefinition;
-import org.apache.commons.lang.text.StrBuilder;
 
 public class MaterialTrackerService implements IMaterialTrackerService {
 
     @Override
     public String checkPipelineForTriggerMaterials(PipelineDefinition pipelineDefinition) {
-        StrBuilder triggerMaterials = new StrBuilder();
-
+        StringBuilder triggerMaterials = new StringBuilder();
+        for (MaterialDefinition materialDefinition : pipelineDefinition.getMaterials()) {
+            if (materialDefinition.isPollingForChanges()) {
+                MaterialUpdater materialUpdater = MaterialUpdaterFactory.create(materialDefinition.getType());
+                MaterialDefinition latestVersion = materialUpdater.getLatestMaterialVersion(materialDefinition);
+                MaterialDefinition dbLatestVersion = this.materialDefinitionService.getLatestMaterialVersion();
+                boolean areTheSame = materialUpdater.areMaterialsSameVersion(latestVersion, dbLatestVersion);
+                if (!areTheSame) {
+                    triggerMaterials.append(materialDefinition.getName());
+                }
+            }
+        }
 
         return triggerMaterials.toString();
     }
