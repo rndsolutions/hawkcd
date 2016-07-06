31bab83b3ea2abdea96aff44ccafbf55969da607
vladislavnikolov <vventsislavov@rnd-solutions.net>
2016-07-06 17:54:16 +0300

    Implemented additional logic #46

--- src/hawkengine/src/main/java/net/hawkengine/core/materialupdater/MaterialPreparerService.java
+++ src/hawkengine/src/main/java/net/hawkengine/core/materialupdater/MaterialPreparerService.java
@@ -1,10 +1,41 @@
 package net.hawkengine.core.materialupdater;
 
+import net.hawkengine.model.Material;
+import net.hawkengine.model.MaterialDefinition;
 import net.hawkengine.model.Pipeline;
+import net.hawkengine.services.MaterialService;
+import net.hawkengine.services.interfaces.IMaterialService;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class MaterialPreparerService implements IMaterialPreparerService {
+    private IMaterialService materialService;
+
+    public MaterialPreparerService() {
+        this.materialService = new MaterialService();
+    }
+
     @Override
     public Pipeline updatePipelineMaterials(Pipeline pipeline) {
+        pipeline.setMaterialsUpdated(true);
+        List<Material> materials = pipeline.getMaterials();
+        for (Material material : materials) {
+            MaterialUpdater materialUpdater = MaterialUpdaterFactory.create(material.getMaterialDefinition().getType());
+            MaterialDefinition latestVersion = materialUpdater.getLatestMaterialVersion(material.getMaterialDefinition());
+            if (latestVersion == null) {
+                pipeline.setMaterialsUpdated(false);
+                break;
+            }
+
+            Material dbLatestVersion = (Material) this.materialService.getLatestMaterial(material.getMaterialDefinition().getId()).getObject();
+            boolean areTheSame = materialUpdater.areMaterialsSameVersion(latestVersion, dbLatestVersion.getMaterialDefinition());
+            if (areTheSame) {
+                material.setMaterialDefinition(latestVersion);
+                material.setChangeDate(LocalDateTime.now());
+            }
+        }
+
         return null;
     }
 }
--- src/hawkengine/src/main/java/net/hawkengine/core/materialupdater/MaterialTrackerService.java
+++ src/hawkengine/src/main/java/net/hawkengine/core/materialupdater/MaterialTrackerService.java
@@ -1,21 +1,36 @@
 package net.hawkengine.core.materialupdater;
 
+import net.hawkengine.model.Material;
 import net.hawkengine.model.MaterialDefinition;
 import net.hawkengine.model.PipelineDefinition;
+import net.hawkengine.services.MaterialService;
+import net.hawkengine.services.interfaces.IMaterialService;
+
+import java.util.List;
 
 public class MaterialTrackerService implements IMaterialTrackerService {
+    private IMaterialService materialService;
+
+    public MaterialTrackerService() {
+        this.materialService = new MaterialService();
+    }
 
     @Override
     public String checkPipelineForTriggerMaterials(PipelineDefinition pipelineDefinition) {
         StringBuilder triggerMaterials = new StringBuilder();
-        for (MaterialDefinition materialDefinition : pipelineDefinition.getMaterials()) {
+        List<MaterialDefinition> materialDefinitions = pipelineDefinition.getMaterials();
+        for (MaterialDefinition materialDefinition : materialDefinitions) {
             if (materialDefinition.isPollingForChanges()) {
                 MaterialUpdater materialUpdater = MaterialUpdaterFactory.create(materialDefinition.getType());
                 MaterialDefinition latestVersion = materialUpdater.getLatestMaterialVersion(materialDefinition);
-                MaterialDefinition dbLatestVersion = this.materialDefinitionService.getLatestMaterialVersion();
-                boolean areTheSame = materialUpdater.areMaterialsSameVersion(latestVersion, dbLatestVersion);
+                if (latestVersion == null) {
+                    continue;
+                }
+
+                Material dbLatestVersion = (Material) this.materialService.getLatestMaterial(materialDefinition.getId()).getObject();
+                boolean areTheSame = materialUpdater.areMaterialsSameVersion(latestVersion, dbLatestVersion.getMaterialDefinition());
                 if (!areTheSame) {
-                    triggerMaterials.append(materialDefinition.getName());
+                    triggerMaterials.append(materialDefinition.getName()).append(" ");
                 }
             }
         }
