295a62c3e015fd186baf12a00bb07ff53c62dcfa
monitobeko <monitobeko@abv.bg>
2016-07-06 11:09:35 +0300

    Small fix to IJobDefinitionService interface

--- src/hawkengine/src/main/java/net/hawkengine/services/interfaces/IJobDefinitionService.java
+++ src/hawkengine/src/main/java/net/hawkengine/services/interfaces/IJobDefinitionService.java
@@ -1,8 +1,9 @@
 package net.hawkengine.services.interfaces;
 
+import net.hawkengine.model.JobDefinition;
 import net.hawkengine.model.ServiceResult;
 
-public interface IJobDefinitionService {
+public interface IJobDefinitionService extends ICrudService<JobDefinition> {
     ServiceResult getAllInStage(String stageDefinitionId);
 
     ServiceResult getAllInPipeline(String pipelineDefinitionId);
