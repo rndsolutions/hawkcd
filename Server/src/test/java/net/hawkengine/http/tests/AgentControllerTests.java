7140e4c5cf1094f8f4183a296ac1053de8cfabdb
Boris Petrov <boris.petrov88@abv.bg>
2016-07-14 16:44:23 +0300

    Implemented AgentControllerTests

--- Server/src/test/java/net/hawkengine/http/tests/AgentControllerTests.java
+++ Server/src/test/java/net/hawkengine/http/tests/AgentControllerTests.java
@@ -1,22 +1,82 @@
 package net.hawkengine.http.tests;
+import com.google.gson.Gson;
+
+
+import net.hawkengine.db.redis.RedisManager;
 import net.hawkengine.http.AgentController;
+import net.hawkengine.model.Agent;
+import net.hawkengine.services.AgentService;
+
+
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import javax.ws.rs.core.Application;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.Application;;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class AgentControllerTests extends JerseyTest {
+    private static  AgentService agentService;
+    private static Gson gson;
+
 
     public Application configure(){
+
         return new ResourceConfig(AgentController.class);
     }
 
-    @Test
-    public void getAllAgentsTest(){
-        Response response = target("/agents").request().get();
-        System.out.println(response);
+    @BeforeClass public static void login() throws IOException, URISyntaxException {
+
+        RedisManager.initializeEmbededDb(6379);
+        RedisManager.connect("localhost");
+        RedisManager.startEmbededDb();
+        gson = new Gson();
+        agentService = new AgentService();
     }
+
+    @AfterClass public static void logout() throws InterruptedException {
+        RedisManager.release();
+        RedisManager.stopEmbededDb();
+
+    }
+
+    @Test
+    public void getAllAngets_request_emptyList(){
+        //Arrange
+        List expectedResult = new ArrayList();
+
+        //Act
+        List<Agent> actualResult = target("/agents").request().get(List.class);
+
+
+        //Assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void getAllAgents_request_oneObject() throws IOException {
+        //Arrange
+        Agent expectedAgent = new Agent();
+
+        //Act
+        Response response = target("/agents").request().get();
+        Agent actualAgent = gson.fromJson(String.valueOf(response.readEntity(List.class).get(0)),Agent.class);
+
+        //Assert
+
+
+
+
+    }
+
+
 }
