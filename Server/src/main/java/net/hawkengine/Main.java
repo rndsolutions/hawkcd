6a70b33b749c328b73bcfb6e1b684222fa826ef1
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-19 16:19:37 +0300

    Implemented logic for server configuration #76

--- Server/src/main/java/net/hawkengine/Main.java
+++ Server/src/main/java/net/hawkengine/Main.java
@@ -1,19 +1,17 @@
 package net.hawkengine;
 
 import net.hawkengine.core.HawkServer;
-import net.hawkengine.db.redis.RedisManager;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
+import net.hawkengine.core.ServerConfiguration;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        HawkServer hawkServer;
-        hawkServer = new HawkServer();
+    public static void main(String[] args) {
         try {
-            hawkServer.configureJetty();
-            hawkServer.start();
+            ServerConfiguration.configure();
+            if (ServerConfiguration.getConfiguration() != null) {
+                HawkServer hawkServer = new HawkServer();
+                hawkServer.configureJetty();
+                hawkServer.start();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
