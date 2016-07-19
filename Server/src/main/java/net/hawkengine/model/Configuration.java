4947811346c9b24687f2dea80e05d78ca1ada0b7
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-19 16:16:38 +0300

    Created Configuration class #76

---
+++ Server/src/main/java/net/hawkengine/model/Configuration.java
@@ -0,0 +1,22 @@
+package net.hawkengine.model;
+
+public final class Configuration {
+    private String serverName;
+    private int serverPort;
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+}
