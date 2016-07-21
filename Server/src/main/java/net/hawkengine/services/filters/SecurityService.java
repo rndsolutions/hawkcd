c53cac9f5cccc5cf5b1b5901f023001772ece607
mvivancheva9 <mvivancheva@gmail.com>
2016-07-21 15:20:45 +0300

    added SecurityService for processing wsContracts #89

---
+++ Server/src/main/java/net/hawkengine/services/filters/SecurityService.java
@@ -0,0 +1,73 @@
+package net.hawkengine.services.filters;
+
+import net.hawkengine.model.ServiceResult;
+import net.hawkengine.model.dto.WsContractDto;
+import net.hawkengine.model.payload.Permission;
+import net.hawkengine.services.filters.interfaces.ISecurityService;
+import net.hawkengine.ws.WsEndpoint;
+import net.hawkengine.ws.WsObjectProcessor;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class SecurityService implements ISecurityService {
+    private static final Logger LOGGER = Logger.getLogger(WsEndpoint.class.getClass());
+    private WsObjectProcessor wsObjectProcessor;
+    private ServiceResult result;
+
+    public SecurityService() {
+        this.wsObjectProcessor = new WsObjectProcessor();
+        this.result = new ServiceResult();
+    }
+
+
+    @Override
+    public ServiceResult getAll(WsContractDto contract, List<Permission> permissions) {
+        try {
+            this.result = (ServiceResult) this.wsObjectProcessor.call(contract);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return this.result;
+    }
+
+    @Override
+    public ServiceResult getById(WsContractDto contract, List<Permission> permissions) {
+        try {
+            this.result = (ServiceResult) this.wsObjectProcessor.call(contract);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return this.result;
+    }
+
+    @Override
+    public ServiceResult add(WsContractDto contract, List<Permission> permissions) {
+        try {
+            this.result = (ServiceResult) this.wsObjectProcessor.call(contract);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return this.result;
+    }
+
+    @Override
+    public ServiceResult update(WsContractDto contract, List<Permission> permissions) {
+        try {
+            this.result = (ServiceResult) this.wsObjectProcessor.call(contract);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return this.result;
+    }
+
+    @Override
+    public ServiceResult delete(WsContractDto contract, List<Permission> permissions) {
+        try {
+            this.result = (ServiceResult) this.wsObjectProcessor.call(contract);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return this.result;
+    }
+}
---
+++ Server/src/main/java/net/hawkengine/services/filters/interfaces/ISecurityService.java
@@ -0,0 +1,19 @@
+package net.hawkengine.services.filters.interfaces;
+
+import net.hawkengine.model.ServiceResult;
+import net.hawkengine.model.dto.WsContractDto;
+import net.hawkengine.model.payload.Permission;
+
+import java.util.List;
+
+public interface ISecurityService {
+    ServiceResult getAll(WsContractDto contract, List<Permission> permissions);
+
+    ServiceResult getById(WsContractDto contract, List<Permission> permissions);
+
+    ServiceResult add(WsContractDto contract, List<Permission> permissions);
+
+    ServiceResult update(WsContractDto contract, List<Permission> permissions);
+
+    ServiceResult delete(WsContractDto contract, List<Permission> permissions);
+}
