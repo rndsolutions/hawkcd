7d827b53f8e9923f78a6252f8d51a8b837557f4c
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-22 13:26:03 +0300

    Created RepositoryFactory class #76

---
+++ Server/src/main/java/net/hawkengine/db/RepositoryFactory.java
@@ -0,0 +1,18 @@
+package net.hawkengine.db;
+
+import net.hawkengine.db.mongodb.MongoDbRepository;
+import net.hawkengine.db.redis.RedisRepository;
+import net.hawkengine.model.enums.DatabaseType;
+
+public class RepositoryFactory {
+    public static IDbRepository create(DatabaseType databaseType, Class classType) {
+        switch (databaseType) {
+            case REDIS:
+                return new RedisRepository(classType);
+            case MONGODB:
+                return new MongoDbRepository(classType);
+            default:
+                return null;
+        }
+    }
+}
