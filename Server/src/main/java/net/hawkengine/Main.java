e582ee3621807a03c2987434636dc8e68eb62dbf
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-22 17:44:17 +0300

    Added logger to Main

--- Server/src/main/java/net/hawkengine/Main.java
+++ Server/src/main/java/net/hawkengine/Main.java
@@ -2,8 +2,11 @@ package net.hawkengine;
 
 import net.hawkengine.core.HawkServer;
 import net.hawkengine.core.ServerConfiguration;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         try {
             ServerConfiguration.configure();
@@ -11,6 +14,8 @@ public class Main {
                 HawkServer hawkServer = new HawkServer();
                 hawkServer.configureJetty();
                 hawkServer.start();
+            } else {
+                LOGGER.error("Failed to load configuration file!");
             }
         } catch (Exception e) {
             e.printStackTrace();
