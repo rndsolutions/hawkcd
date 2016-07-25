520f9aed3a37d74256246a36dff9de04af15ef34
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-25 18:56:45 +0300

    Refactoring #76

--- Server/src/main/java/net/hawkengine/Main.java
+++ Server/src/main/java/net/hawkengine/Main.java
@@ -5,17 +5,18 @@ import net.hawkengine.core.ServerConfiguration;
 import org.apache.log4j.Logger;
 
 public class Main {
+    private static final String FAILED_TO_LOAD_CONFIG_ERROR = "Failed to load configuration file: ";
     private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
         try {
-            ServerConfiguration.configure();
-            if (ServerConfiguration.getConfiguration() != null) {
+            String errorMessage = ServerConfiguration.configure();
+            if (errorMessage.isEmpty()) {
                 HawkServer hawkServer = new HawkServer();
                 hawkServer.configureJetty();
                 hawkServer.start();
             } else {
-                LOGGER.error("Failed to load configuration file!");
+                LOGGER.error(FAILED_TO_LOAD_CONFIG_ERROR + errorMessage);
             }
         } catch (Exception e) {
             e.printStackTrace();
--- Server/src/main/java/net/hawkengine/core/ServerConfiguration.java
+++ Server/src/main/java/net/hawkengine/core/ServerConfiguration.java
@@ -1,47 +1,126 @@
 package net.hawkengine.core;
 
-import net.hawkengine.core.utilities.constants.ConfigurationConstants;
 import net.hawkengine.model.configuration.Configuration;
+import net.hawkengine.model.configuration.DatabaseConfig;
+import net.hawkengine.model.enums.DatabaseType;
 import org.apache.commons.io.FileUtils;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.error.YAMLException;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.hawkengine.core.utilities.constants.ConfigurationConstants.*;
 
 public class ServerConfiguration {
     private static Configuration configuration;
-
-    public static void configure() {
-        configuration = generateConfiguration();
-    }
 
     public static Configuration getConfiguration() {
         return configuration;
     }
 
-    private static Configuration generateConfiguration() {
-        File configFile = new File(ConfigurationConstants.CONFIGURATION_FILE_NAME);
-        if (!configFile.exists()) {
-            createConfigFile(configFile);
+    public static String configure() {
+        File configFile = new File(CONFIG_FILE_NAME);
+        String errorMessage = createConfigFile(configFile);
+        if (!errorMessage.isEmpty()) {
+            return errorMessage;
         }
 
-        Configuration configuration = null;
-        try {
-            Yaml yaml = new Yaml();
-            FileInputStream fileInputStream = new FileInputStream(configFile);
-            configuration = yaml.loadAs(fileInputStream, Configuration.class);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        errorMessage = loadConfiguration(configFile);
+        if (!errorMessage.isEmpty()) {
+            return errorMessage;
         }
 
-        return configuration;
+        errorMessage = validateConfiguration(configuration);
+        return errorMessage;
     }
 
-    private static void createConfigFile(File configFile) {
-        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(configFile.getName());
-        try {
-            FileUtils.copyInputStreamToFile(stream, configFile);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static String createConfigFile(File configFile) {
+        String errorMessage = "";
+        if (!configFile.exists()) {
+            InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(CONFIG_FILE_NAME);
+            try {
+                FileUtils.copyInputStreamToFile(stream, configFile);
+            } catch (IOException e) {
+                errorMessage = String.format(FAILED_TO_CREATE_CONFIG, configFile.getName());
+            }
         }
+
+        return errorMessage;
+    }
+
+    public static String loadConfiguration(File configFile) {
+        String errorMessage = "";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(configFile);
+
+            Yaml yaml = new Yaml();
+            configuration = yaml.loadAs(fileInputStream, Configuration.class);
+        } catch (FileNotFoundException e) {
+            errorMessage = String.format(FAILED_TO_LOCATE_CONFIG, configFile.getName());
+        } catch (YAMLException e) {
+            errorMessage = e.getMessage();
+            String pattern = "property=(.*?)\\s";
+            Pattern regex = Pattern.compile(pattern);
+            Matcher matcher = regex.matcher(errorMessage);
+            if (matcher.find()) {
+                errorMessage = String.format(INVALID_CONFIG_PROPERTY, matcher.group(1));
+            }
+        }
+
+        return errorMessage;
+    }
+
+    public static String validateConfiguration(Configuration configuration) {
+        StringBuilder errorMessage = new StringBuilder();
+
+        // Server settings
+        String serverHost = configuration.getServerHost();
+        if (serverHost == null || serverHost.isEmpty()) {
+            errorMessage.append(String.format(EMPTY_CONFIG_PROPERTY, PROPERTY_SERVER_HOST));
+        }
+
+        // Database settings
+        DatabaseType databaseType = configuration.getDatabaseType();
+        DatabaseConfig databaseConfig = configuration.getDatabaseConfigs().get(databaseType);
+
+        String name = databaseConfig.getName();
+        if (name == null || name.isEmpty()) {
+            errorMessage.append(DATABASE_CONFIG_MESSAGE);
+            errorMessage.append(String.format(EMPTY_CONFIG_PROPERTY, PROPERTY_DATABASE_NAME));
+        }
+
+        String host = databaseConfig.getHost();
+        if (host == null || host.isEmpty()) {
+            errorMessage.append(DATABASE_CONFIG_MESSAGE);
+            errorMessage.append(String.format(EMPTY_CONFIG_PROPERTY, PROPERTY_DATABASE_HOST));
+        }
+
+        // Material settings
+        String materialsDestination = configuration.getMaterialsDestination();
+        if (materialsDestination == null || materialsDestination.isEmpty()) {
+            errorMessage.append(String.format(EMPTY_CONFIG_PROPERTY, PROPERTY_MATERIALS_DESTINATION));
+        }
+
+        String artifactsDestination = configuration.getArtifactsDestination();
+        if (artifactsDestination == null || artifactsDestination.isEmpty()) {
+            errorMessage.append(String.format(EMPTY_CONFIG_PROPERTY, PROPERTY_ARTIFACTS_DESTINATION));
+        }
+
+        // Worker settings
+        int pipelineSchedulerPollInterval = configuration.getPipelineSchedulerPollInterval();
+        if (pipelineSchedulerPollInterval < MIN_WORKER_POLL_INTERVAL ||
+                pipelineSchedulerPollInterval > MAX_WORKER_POLL_INTERVAL) {
+            errorMessage.append(String.format(WORKER_POLL_INTERVAL_ERROR, PROPERTY_SCHEDULER_POLL_INTERVAL, MIN_WORKER_POLL_INTERVAL, MAX_WORKER_POLL_INTERVAL));
+        }
+
+        int materialTrackerPollInterval = configuration.getMaterialTrackerPollInterval();
+        if (materialTrackerPollInterval < MIN_WORKER_POLL_INTERVAL ||
+                materialTrackerPollInterval > MAX_WORKER_POLL_INTERVAL) {
+            errorMessage.append(String.format(WORKER_POLL_INTERVAL_ERROR, PROPERTY_TRACKER_POLL_INTERVAL, MIN_WORKER_POLL_INTERVAL, MAX_WORKER_POLL_INTERVAL));
+        }
+
+        return errorMessage.toString();
     }
 }
