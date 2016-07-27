452dd122010402803479b3b58cf3b38f18936631
hpslavov <slavov.hristo@gmail.com>
2016-07-27 11:35:30 +0300

    Fixed agent status colors. Fixed bug due to agent reporting status #106

--- Server/src/main/java/net/hawkengine/http/AgentController.java
+++ Server/src/main/java/net/hawkengine/http/AgentController.java
@@ -194,6 +194,7 @@ public class AgentController {
             agentFromDb.setIpAddress(agent.getIpAddress());
             agentFromDb.setRootPath(agent.getRootPath());
             agentFromDb.setName(agent.getName());
+            agentFromDb.setRunning(agent.isRunning());
 
             result = this.agentService.update(agentFromDb);
 
