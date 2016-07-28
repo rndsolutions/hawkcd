efe971a7c2bf132c562a2e4a0af9b8319e9e1866
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-28 18:13:40 +0300

    Created ServerMessages class #118

---
+++ Server/src/main/java/net/hawkengine/core/utilities/constants/ServerMessages.java
@@ -0,0 +1,8 @@
+package net.hawkengine.core.utilities.constants;
+
+public class ServerMessages {
+    public static final String RETRIEVED_SUCCESSFULLY = "retrieved successfully";
+    public static final String NOT_FOUND = "not found";
+    public static final String COULD_NOT_BE_DELETED = "could not be deleted because a FetchMaterial Task in Pipeline %s is using it";
+
+}
