9dffed532540c331bdf7e59acb9ac772828e0da1
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-07-29 18:06:59 +0300

    Refactoring and new logic implemented #118

--- Server/src/main/java/net/hawkengine/services/MaterialDefinitionService.java
+++ Server/src/main/java/net/hawkengine/services/MaterialDefinitionService.java
@@ -4,7 +4,6 @@ import net.hawkengine.core.utilities.constants.ServerMessages;
 import net.hawkengine.db.DbRepositoryFactory;
 import net.hawkengine.db.IDbRepository;
 import net.hawkengine.model.*;
-import net.hawkengine.model.enums.TaskType;
 import net.hawkengine.services.interfaces.IMaterialDefinitionService;
 import net.hawkengine.services.interfaces.IPipelineDefinitionService;
 
@@ -41,12 +40,15 @@ public class MaterialDefinitionService extends CrudService<MaterialDefinition> i
 
     @Override
     public ServiceResult getAllFromPipelineDefinition(String pipelineDefinitionId) {
-        PipelineDefinition pipelineDefinition = (PipelineDefinition) this.pipelineDefinitionService.getById(pipelineDefinitionId).getObject();
         List<MaterialDefinition> materialDefinitions = new ArrayList<>();
-        for (String materialDefinitionId : pipelineDefinition.getMaterialDefinitionIds()) {
-            MaterialDefinition materialDefinition = (MaterialDefinition) this.getById(materialDefinitionId).getObject();
-            if (materialDefinition != null) {
-                materialDefinitions.add(materialDefinition);
+
+        PipelineDefinition pipelineDefinition = (PipelineDefinition) this.pipelineDefinitionService.getById(pipelineDefinitionId).getObject();
+        if (pipelineDefinition != null) {
+            for (String materialDefinitionId : pipelineDefinition.getMaterialDefinitionIds()) {
+                MaterialDefinition materialDefinition = (MaterialDefinition) this.getById(materialDefinitionId).getObject();
+                if (materialDefinition != null) {
+                    materialDefinitions.add(materialDefinition);
+                }
             }
         }
 
@@ -85,38 +87,21 @@ public class MaterialDefinitionService extends CrudService<MaterialDefinition> i
 
     @Override
     public ServiceResult delete(String materialDefinitionId) {
-        MaterialDefinition materialDefinition = (MaterialDefinition) this.getById(materialDefinitionId).getObject();
-        if (materialDefinition == null) {
-            return super.createServiceResult(materialDefinition, true, ServerMessages.NOT_FOUND);
-        }
-
-        List<TaskDefinition> taskDefinitions = null;
-        for (TaskDefinition taskDefinition : taskDefinitions) {
-            TaskType taskType = taskDefinition.getType();
-            if (taskType == TaskType.FETCH_MATERIAL) {
-                FetchMaterialTask fetchMaterialTask = (FetchMaterialTask) taskDefinition;
-                String materialName = fetchMaterialTask.getMaterialName();
-                if (materialDefinition.getName().equals(materialName)) {
-                    return super.createServiceResult(null, true, String.format(ServerMessages.COULD_NOT_BE_DELETED, fetchMaterialTask.getPipelineName()));
-                }
-            }
-        }
-
-        ServiceResult result = super.delete(materialDefinitionId);
-        if (result.hasError()) {
-            return result;
-        }
-
         List<PipelineDefinition> pipelineDefinitions = (List<PipelineDefinition>) this.pipelineDefinitionService.getAll().getObject();
+        List<String> assignedIds = new ArrayList<>();
         for (PipelineDefinition pipelineDefinition : pipelineDefinitions) {
-            List<String> materialDefinitionIds = pipelineDefinition.getMaterialDefinitionIds();
-            for (String definitionId : materialDefinitionIds) {
-                if (materialDefinitionId.equals(definitionId)) {
-                    materialDefinitionIds.remove(definitionId);
+            for (String assignedId : pipelineDefinition.getMaterialDefinitionIds()) {
+                if (assignedId.equals(materialDefinitionId)) {
+                    assignedIds.add(assignedId);
                 }
             }
         }
 
-        return result;
+        if (!assignedIds.isEmpty()) {
+            String assignedIdsAsString = String.join(", ", assignedIds);
+            return super.createServiceResult(null, true, String.format(ServerMessages.COULD_NOT_BE_DELETED, assignedIdsAsString));
+        }
+
+        return super.delete(materialDefinitionId);
     }
 }
