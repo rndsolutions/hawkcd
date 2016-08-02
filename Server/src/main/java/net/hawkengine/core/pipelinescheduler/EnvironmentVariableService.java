bbda6978387118a28a238c6993c565576da2a3a3
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-08-02 17:45:04 +0300

    Created EnvironmentVariableService #126

---
+++ Server/src/main/java/net/hawkengine/core/pipelinescheduler/EnvironmentVariableService.java
@@ -0,0 +1,55 @@
+package net.hawkengine.core.pipelinescheduler;
+
+import net.hawkengine.model.EnvironmentVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnvironmentVariableService {
+    public List<EnvironmentVariable> getOverridenVariables(
+            List<EnvironmentVariable> jobVariables, List<EnvironmentVariable> stageVariables, List<EnvironmentVariable> pipelineVariables) {
+        List<EnvironmentVariable> overriddenVariables = new ArrayList<>();
+        for (EnvironmentVariable variable : jobVariables) {
+            boolean variableExists = overriddenVariables.stream().anyMatch(v -> v.getKey().equals(variable.getKey()));
+            if (!variableExists) {
+                overriddenVariables.add(variable);
+            }
+        }
+
+        for (EnvironmentVariable variable : stageVariables) {
+            boolean variableExists = overriddenVariables.stream().anyMatch(v -> v.getKey().equals(variable.getKey()));
+            if (!variableExists) {
+                overriddenVariables.add(variable);
+            }
+        }
+
+        for (EnvironmentVariable variable : pipelineVariables) {
+            boolean variableExists = overriddenVariables.stream().anyMatch(v -> v.getKey().equals(variable.getKey()));
+            if (!variableExists) {
+                overriddenVariables.add(variable);
+            }
+        }
+
+        return overriddenVariables;
+    }
+
+    public String replaceVariablesInArguments(String arguments, List<EnvironmentVariable> variables) {
+        String result = arguments;
+
+        int startIndex = arguments.indexOf('%');
+        int endIndex = arguments.indexOf('%', startIndex + 1);
+        while (startIndex >= 0 && endIndex > startIndex) {
+            String variableToReplace = arguments.substring(startIndex, endIndex + 1);
+            String variableKey = arguments.substring(startIndex + 1, endIndex);
+            EnvironmentVariable currentVariable = variables.stream().filter(v -> v.getKey().equals(variableKey)).findFirst().orElse(null);
+            if (currentVariable != null) {
+                result = result.replace(variableToReplace, currentVariable.getValue());
+            }
+
+            startIndex = arguments.indexOf('%', endIndex + 1);
+            endIndex = arguments.indexOf('%', startIndex + 1);
+        }
+
+        return result;
+    }
+}
