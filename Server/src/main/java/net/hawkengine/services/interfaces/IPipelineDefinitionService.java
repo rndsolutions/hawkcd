3ec21c64f7c0c4cc3e9cfc529fd89a2d4d1f5d71
mvivancheva9 <mvivancheva@gmail.com>
2016-08-05 09:10:06 +0300

    refactored assignPipelineToGroup and unassignPipelineFromGroup #89

--- Server/src/main/java/net/hawkengine/services/interfaces/IPipelineDefinitionService.java
+++ Server/src/main/java/net/hawkengine/services/interfaces/IPipelineDefinitionService.java
@@ -5,6 +5,7 @@
 package net.hawkengine.services.interfaces;
 
 import net.hawkengine.model.PipelineDefinition;
+import net.hawkengine.model.PipelineGroup;
 import net.hawkengine.model.ServiceResult;
 
 public interface IPipelineDefinitionService extends ICrudService<PipelineDefinition> {
@@ -12,5 +13,5 @@ public interface IPipelineDefinitionService extends ICrudService<PipelineDefinit
 
     ServiceResult unassignPipelineFromGroup(PipelineDefinition pipelineDefinition);
 
-    ServiceResult assignPipelineToGroup(PipelineDefinition pipelineDefinition);
+    ServiceResult assignPipelineToGroup(PipelineDefinition pipelineDefinition, PipelineGroup pipelineGroup);
 }
