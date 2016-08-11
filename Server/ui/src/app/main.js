7710239f48ca0072690f417fc5abef5345e1ccf1
hpslavov <slavov.hristo@gmail.com>
2016-08-11 13:09:43 +0300

    Small fixes #110

--- Server/ui/src/app/main.js
+++ Server/ui/src/app/main.js
@@ -19,7 +19,7 @@ angular
         //"ngSanitize",
         'luegg.directives',
         'toaster',
-        'momentjs'
+        'momentjs',
         'satellizer'
         //'ngAnimate'
         //'flow'
