5b81d8ad5dbe3efa6f39b046df717433bdf70b5e
mvivancheva9 <mvivancheva@gmail.com>
2016-08-15 13:11:48 +0300

    added services for stage,job and task definitions #95

--- Server/src/main/java/net/hawkengine/services/filters/factories/AuthorizationServiceFactory.java
+++ Server/src/main/java/net/hawkengine/services/filters/factories/AuthorizationServiceFactory.java
@@ -9,10 +9,16 @@ public class AuthorizationServiceFactory {
     public static IAuthorizationService create(String service) {
         switch (service) {
             case "PipelineDefinitionService":
-            case "StageDefinitionService":
-            case "JobDefinitionService":
-            case "TaskDefinitionService":
                 authorizationService = new PipelineDefinitionAuthorizationService();
+                return authorizationService;
+            case "StageDefinitionService":
+                authorizationService = new StageDefinitionAuthorizationService();
+                return authorizationService;
+            case "JobDefinitionService":
+                authorizationService = new JobDefinitionAuthorizationService();
+                return authorizationService;
+            case "TaskDefinitionService":
+                authorizationService = new TaskDefinitionAuthorizationService();
                 return authorizationService;
             case "AgentService":
                 authorizationService = new AgentAuthorizationService();
