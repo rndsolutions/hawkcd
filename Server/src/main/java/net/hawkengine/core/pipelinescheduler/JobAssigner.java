2df93d6e7d9cf706765143f504cd3c36cd2c1da2
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-08-31 17:33:56 +0300

    Refactored JobAssigned and it's services #194

--- Server/src/main/java/net/hawkengine/core/pipelinescheduler/JobAssigner.java
+++ Server/src/main/java/net/hawkengine/core/pipelinescheduler/JobAssigner.java
@@ -2,29 +2,25 @@ package net.hawkengine.core.pipelinescheduler;
 
 import net.hawkengine.core.ServerConfiguration;
 import net.hawkengine.core.utilities.constants.LoggerMessages;
-import net.hawkengine.model.*;
-import net.hawkengine.model.enums.StageStatus;
+import net.hawkengine.model.Agent;
 import net.hawkengine.services.AgentService;
-import net.hawkengine.services.PipelineService;
 import net.hawkengine.services.interfaces.IAgentService;
-import net.hawkengine.services.interfaces.IPipelineService;
-import net.hawkengine.ws.EndpointConnector;
 import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class JobAssigner implements Runnable {
-    private IAgentService agentService;
-    private IPipelineService pipelineService;
+    private static final Logger LOGGER = Logger.getLogger(PipelinePreparer.class.getName());
+    private static final int POLL_INTERVAL = ServerConfiguration.getConfiguration().getPipelineSchedulerPollInterval() * 1000;
+
     private JobAssignerService jobAssignerService;
     private StatusUpdaterService statusUpdaterService;
-    private static final Logger LOGGER = Logger.getLogger(PipelinePreparer.class.getName());
+    private IAgentService agentService;
 
     public JobAssigner() {
-        this.agentService = new AgentService();
-        this.pipelineService = new PipelineService();
         this.jobAssignerService = new JobAssignerService();
         this.statusUpdaterService = new StatusUpdaterService();
+        this.agentService = new AgentService();
     }
 
     @Override
@@ -32,30 +28,14 @@ public class JobAssigner implements Runnable {
         LOGGER.info(String.format(LoggerMessages.WORKER_STARTED, this.getClass().getSimpleName()));
         try {
             while (true) {
+                List<Agent> agents = (List<Agent>) this.agentService.getAll().getObject();
+
                 this.statusUpdaterService.updateStatuses();
-                List<Agent> agents = (List<Agent>) this.agentService.getAllAssignableAgents().getObject();
-                List<Pipeline> pipelines = (List<Pipeline>) this.pipelineService.getAllPreparedPipelinesInProgress().getObject();
+                this.jobAssignerService.checkUnassignedJobs(agents);
+                this.jobAssignerService.checkAwaitingJobs(agents);
+                this.jobAssignerService.assignJobs(agents);
 
-                for (Pipeline pipeline : pipelines) {
-                    for (Stage stage : pipeline.getStages()) {
-                        if (stage.getStatus() == StageStatus.IN_PROGRESS) {
-                            for (Job job : stage.getJobs()) {
-                                if (agents.size() != 0) {
-                                    Agent agent = this.jobAssignerService.assignAgentToJob(job, agents);
-                                    if (agent != null) {
-                                        ServiceResult result = this.agentService.update(agent);
-                                        EndpointConnector.passResultToEndpoint(AgentService.class.getSimpleName(), "update", result);
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    ServiceResult result = this.pipelineService.update(pipeline);
-                    EndpointConnector.passResultToEndpoint(PipelineService.class.getSimpleName(), "update", result);
-                }
-
-                Thread.sleep(ServerConfiguration.getConfiguration().getMaterialTrackerPollInterval() * 1000);
+                Thread.sleep(POLL_INTERVAL);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
