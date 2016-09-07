c672854d6585df7483d81d03e063bfdcfc6306e6
hpslavov <slavov.hristo@gmail.com>
2016-09-07 15:16:27 +0300

    Added logic for converting utc time in local #232

--- Server/ui/src/app/application/services/utility/moment.js
+++ Server/ui/src/app/application/services/utility/moment.js
@@ -1,25 +1,52 @@
 'use strict';
 
-angular.module('momentjs',[])
-  .factory('moment',['$window',function($window){
-      var moment = $window.moment;
-      moment.getLastRunAction = function(pipelineRun) {
-          if (pipelineRun.endTime == undefined) {
-              return;
-          }
-          var result = {};
-          var runEndTime = pipelineRun.endTime;
-          var delta = moment(runEndTime);
-          var now = moment();
-          var diff = moment.duration(moment(now).diff(moment(delta))).humanize();
-          if(diff == 'a few seconds'){
-              diff = 'few seconds ago';
-              result.output = diff;
-          } else {
-              result.output = diff + " ago";
-          }
-          return result;
-      };
+angular.module('momentjs', [])
+    .factory('moment', ['$window', function($window) {
+        var moment = $window.moment;
+        moment.getLastRunAction = function(pipelineRun) {
+            if (pipelineRun.endTime == undefined) {
+                return;
+            }
+            var result = {};
+            var endDay = pipelineRun.endTime.date;
+            var endTime = pipelineRun.endTime.time;
+            var runEndTime = pipelineRun.endTime;
+            debugger;
+            var time = moment.utc({
+                year: endDay.year,
+                month: endDay.month,
+                day: endDay.day,
+                hour: endTime.hour,
+                minute: endTime.minute,
+                second: endTime.second
+            });
+            //date is in UTC. 3 hours back. To be in local we add 3 hours. We also add 1 month, because JS months in date are -1.[arrays count]
+            // time.add(3, 'h');
+            time.local();
+            time.subtract(1, 'M');
+            var delta = moment(time);
+            var now = moment();
+            var diff = moment.duration(moment(now).diff(moment(delta))).humanize();
+            var rawDiff = moment.duration(moment(now).diff(moment(delta)));
+            if (rawDiff.hours() < 1) {
+                diff = rawDiff.minutes() + ' minutes ago';
+                result.output = diff;
+            } else {
+                result.output = diff + " ago";
+            }
+            return result;
+        };
 
-      return moment;
-  }]);
+        moment.formatTimeInLocal = function(time) {
+            var bufferTime = moment.utc(time);
+            bufferTime.local();
+            var result = {
+              hour: bufferTime.hours(),
+              minute:bufferTime.minutes(),
+              second:bufferTime.seconds()
+            };
+            return result;
+        };
+      
+        return moment;
+    }]);
