193d57399941e49b63bf91113ec8c162b9aa2130
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-09-27 10:18:51 +0300

    Added id field to PipelineGroupDto #316

--- Server/src/main/java/net/hawkengine/model/dto/PipelineGroupDto.java
+++ Server/src/main/java/net/hawkengine/model/dto/PipelineGroupDto.java
@@ -1,20 +1,14 @@
 package net.hawkengine.model.dto;
 
 import net.hawkengine.model.PermissionObject;
+import net.hawkengine.model.PipelineGroup;
 
 import java.util.List;
 
 public class PipelineGroupDto extends PermissionObject {
+    private String id;
     private String name;
     private List<PipelineDefinitionDto> pipelines;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public List<PipelineDefinitionDto> getPipelines() {
         return pipelines;
@@ -22,5 +16,11 @@ public class PipelineGroupDto extends PermissionObject {
 
     public void setPipelines(List<PipelineDefinitionDto> pipelines) {
         this.pipelines = pipelines;
+    }
+
+    public void constructDto(PipelineGroup pipelineGroup) {
+        this.setPermissionType(pipelineGroup.getPermissionType());
+        this.id = pipelineGroup.getId();
+        this.name = pipelineGroup.getName();
     }
 }
