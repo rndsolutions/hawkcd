07b2e7cb7b420bfcd2425c2094fad02b8e8686a2
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-10-04 13:30:56 +0300

    Implemented additional logic in constructDto method #358

--- Server/src/main/java/net/hawkengine/model/dto/PipelineDefinitionDto.java
+++ Server/src/main/java/net/hawkengine/model/dto/PipelineDefinitionDto.java
@@ -4,20 +4,31 @@ import net.hawkengine.model.PermissionObject;
 import net.hawkengine.model.Pipeline;
 import net.hawkengine.model.PipelineDefinition;
 
-public class PipelineDefinitionDto extends PermissionObject{
+import java.util.ArrayList;
+import java.util.List;
+
+public class PipelineDefinitionDto extends PermissionObject {
     private String id;
     private String name;
     private PipelineDto lastRun;
+    private List<Integer> pipelineExecutionIds;
 
     public PipelineDefinitionDto() {
         this.lastRun = new PipelineDto();
+        this.pipelineExecutionIds = new ArrayList<>();
     }
 
-    public void constructDto(PipelineDefinition pipelineDefinition, Pipeline pipeline) {
+    public void constructDto(PipelineDefinition pipelineDefinition, List<Pipeline> pipelines) {
         this.id = pipelineDefinition.getId();
         this.name = pipelineDefinition.getName();
-        if (pipeline != null) {
-            this.lastRun.constructBasePipelineDto(pipeline);
+        if (!pipelines.isEmpty()) {
+            int numberOfPipelines = pipelines.size();
+            for (int i = 1; i <= numberOfPipelines; i++) {
+                this.pipelineExecutionIds.add(pipelines.get(i).getExecutionId());
+                if (i == numberOfPipelines) {
+                    this.lastRun.constructBasePipelineDto(pipelines.get(i));
+                }
+            }
         } else {
             this.lastRun.constructEmptyPipelineDto(pipelineDefinition);
         }
