c074e2e79aa925769dfa4e131faf3a1f33bbdea1
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-10-14 17:49:32 +0300

    Created RevisionService

---
+++ Server/src/main/java/net/hawkengine/services/RevisionService.java
@@ -0,0 +1,64 @@
+package net.hawkengine.services;
+
+import net.hawkengine.db.DbRepositoryFactory;
+import net.hawkengine.db.IDbRepository;
+import net.hawkengine.model.PipelineDefinition;
+import net.hawkengine.model.PipelineDefinitionRevision;
+import net.hawkengine.model.Revision;
+import net.hawkengine.model.ServiceResult;
+import net.hawkengine.services.interfaces.IRevisionService;
+
+import java.util.List;
+
+public class RevisionService extends CrudService<Revision> implements IRevisionService {
+    private static final Class CLASS_TYPE = Revision.class;
+
+    public RevisionService() {
+        IDbRepository repository = DbRepositoryFactory.create(DATABASE_TYPE, CLASS_TYPE);
+        super.setRepository(repository);
+        super.setObjectType(CLASS_TYPE.getSimpleName());
+    }
+
+    @Override
+    public ServiceResult getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public ServiceResult add(Revision revision) {
+        return super.add(revision);
+    }
+
+    @Override
+    public ServiceResult update(Revision revision) {
+        return super.update(revision);
+    }
+
+    public Revision addRevisionOfPipelineDefinition(PipelineDefinition pipelineDefinition) {
+        PipelineDefinitionRevision pipelineDefinitionRevision = new PipelineDefinitionRevision(pipelineDefinition);
+
+        Revision revision = this.getByPipelineDefinitionId(pipelineDefinition.getId());
+        if (revision != null) {
+            revision.getRevisions().add(pipelineDefinitionRevision);
+            this.update(revision);
+        } else {
+            revision = new Revision(pipelineDefinition.getId(), pipelineDefinition.getName());
+            revision.getRevisions().add(pipelineDefinitionRevision);
+            this.add(revision);
+        }
+
+        return revision;
+    }
+
+    public Revision getByPipelineDefinitionId(String pipelineDefinitionId) {
+        Revision result = null;
+        List<Revision> revisions = (List<Revision>) this.getAll().getObject();
+        for (Revision revision : revisions) {
+            if (revision.getPipelineDefinitionId().equals(pipelineDefinitionId)) {
+                result = revision;
+            }
+        }
+
+        return result;
+    }
+}
