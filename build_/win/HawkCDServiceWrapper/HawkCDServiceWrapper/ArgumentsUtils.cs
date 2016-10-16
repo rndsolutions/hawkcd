70c3b30271b9a1845715be3bf80f1467f15f4c80
Slavi Dimitrov <sdimitrov@rnd-solutions.net>
2016-10-16 23:02:13 +0300

    Adding the HawkCDServiceWrapper project.  #365

---
+++ build_/win/HawkCDServiceWrapper/HawkCDServiceWrapper/ArgumentsUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+
+namespace HawkCDServiceWrapper
+{
+    public class ArgumentsUtils
+    {
+        public static string StartProcessName { get; private set; }
+        public static string StartArgs { get; private set; }
+        public static string StopArgs { get; private set; }
+        public static string EventSourceName { get; private set; }
+        public static string WorkingDirectory { get; private set; }
+        public static bool ReadArgs()
+        {
+            try
+            {
+                StartProcessName = ConfigurationManager.AppSettings["StartProcessName"];
+                StartArgs = ConfigurationManager.AppSettings["StartArgs"];
+                EventSourceName = ConfigurationManager.AppSettings["EventSourceName"];
+                WorkingDirectory = ConfigurationManager.AppSettings["WorkingDirectory"];
+                StopArgs = ConfigurationManager.AppSettings["StopArgs"];
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ build_/win/HawkCDServiceWrapper/HawkCDServiceWrapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HawkCDServiceWrapper
+{
+    static class Program
+    {
+        //http://stackoverflow.com/questions/3663331/creating-a-service-with-sc-exe-how-to-pass-in-context-parameters
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new HawkService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
