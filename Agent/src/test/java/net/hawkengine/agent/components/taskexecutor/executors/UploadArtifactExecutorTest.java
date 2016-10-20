07f2b1d94ccad7c4efc47cf1296a6e98fde1ec38
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-10-20 13:35:46 +0300

    Applied Apache license to all Agent files

--- Agent/src/test/java/net/hawkengine/agent/components/taskexecutor/executors/UploadArtifactExecutorTest.java
+++ Agent/src/test/java/net/hawkengine/agent/components/taskexecutor/executors/UploadArtifactExecutorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 R&D Solutions Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.hawkengine.agent.components.taskexecutor.executors;
 
 import com.google.gson.Gson;
@@ -5,9 +21,7 @@ import com.google.gson.GsonBuilder;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-
 import junit.framework.Assert;
-
 import net.hawkengine.agent.AgentConfiguration;
 import net.hawkengine.agent.base.TestBase;
 import net.hawkengine.agent.enums.TaskStatus;
@@ -20,7 +34,6 @@ import net.hawkengine.agent.models.payload.UploadArtifactInfo;
 import net.hawkengine.agent.models.payload.WorkInfo;
 import net.hawkengine.agent.services.FileManagementService;
 import net.hawkengine.agent.services.interfaces.IFileManagementService;
-
 import net.hawkengine.agent.utilities.deserializers.TaskDefinitionAdapter;
 import org.junit.Before;
 import org.junit.Ignore;
