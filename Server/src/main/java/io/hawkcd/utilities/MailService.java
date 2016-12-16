619531f860d122c7667be81196adb6f3fa6ab377
vladislavnikolov <vladislav.v.nikolov@gmail.com>
2016-12-16 17:17:04 +0200

    Created MailService class #443

---
+++ Server/src/main/java/io/hawkcd/utilities/MailService.java
@@ -0,0 +1,56 @@
+package io.hawkcd.utilities;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailService {
+    public static void sendMail() {
+        // Recipient's email ID needs to be mentioned.
+        String to = "vventsislavov@rnd-solutions.net";
+
+        // Sender's email ID needs to be mentioned
+        String from = "vladislav.v.nikolov@gmail.com";
+
+        // Assuming you are sending email from localhost
+        String host = "localhost";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is actual message");
+
+            // Send message
+//            Transport transport = session.getTransport("smtp");
+//            transport.connect(null, login, pass);
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
