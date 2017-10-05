6f191fb88ff1a2e1b39c2c570dfff51b65d20e7b
stochev <tochev86@gmail.com>
2017-10-05 17:03:45 +0300

    HAWKCD-22
    	-The remove run button applied from front-end perspective.
    	-In order to be fully functional, we need back-end endpoint.

--- Server/ui/src/app/pipelines/services/pipelineHistory.service.js
+++ Server/ui/src/app/pipelines/services/pipelineHistory.service.js
@@ -34,5 +34,18 @@ angular
             websocketSenderService.call(json);
         };
 
+        pipeHistoryService.deleteRun = function (pipelineDefinitionId, runToDelete) {
+            // var methodName = "deleteRun";
+            // var className = "PipelineService";
+            // var packageName = "io.hawkcd.services";
+            // var result = "";
+            // var args = ["{\"packageName\": \"java.lang.String\", \"object\": \"" + pipelineDefinitionId + "\"}",
+            //     "{\"packageName\": \"java.lang.Integer\", \"object\": " + numberOfPipelines + "}",
+            //     "{\"packageName\": \"java.lang.String\", \"object\": \"" + pipelineId + "\"}"];
+            // var error = "";
+            // var json = jsonHandlerService.createJson(className, packageName, methodName, result, error, args);
+            // websocketSenderService.call(json);
+        };
+
         return pipeHistoryService;
     }]);
